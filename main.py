65c4696922c6f27c07d677e262efa12ee5ea729c
aseemraj <aseem.mathswiz@gmail.com>
2014-08-27 23:24:06 +0530

    Class structure added

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
 import sys
 from PyQt4 import QtCore, QtGui
 
+WIDTH = 1300
+HEIGHT = 700
+
+class MainWin(QtGui.QWidget):
+    
+    def __init__(self):
+        super(MainWin, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        
+        self.resize(WIDTH, HEIGHT)
+        self.setWindowTitle('Train Traffic Simulator')
+
+        # Time slider
+        slider = QtGui.QSlider(1, self)
+        slider.setRange(0, 2359)
+        slider.setSingleStep(10)
+        slider.resize(500, 20)
+        slider.move(200, 57)
+
+        # Start button
+        sbtn = QtGui.QPushButton('Start Simulation', self)
+        sbtn.setToolTip('Click to start simulation')
+        sbtn.resize(sbtn.sizeHint())
+        sbtn.move(50, 50)
+        
+        # Quit button
+        qbtn = QtGui.QPushButton('Quit', self)
+        qbtn.setToolTip('Quit Application')
+        qbtn.resize(sbtn.sizeHint())
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.move(WIDTH-200, HEIGHT-70)
+
+        self.show()
+        
+    def closeEvent(self, event):
+        
+        reply = QtGui.QMessageBox.question(self, 'Confirmation',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes | 
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
 def main():
     app = QtGui.QApplication(sys.argv)
     
     # create the screen object
     screen = QtGui.QDesktopWidget().screenGeometry()
-    WIDTH = 1300
-    HEIGHT = 700
-    
-    
-    # assign height and width of desktop screen to the widget
-    w = QtGui.QWidget()
-    w.resize(WIDTH,HEIGHT)
-    w.setWindowTitle('Train Traffic Simulator')
 
-    # Time slider
-    slider = QtGui.QSlider(1, w)
-    slider.setRange(0, 2359)
-    slider.setSingleStep(10)
-    slider.resize(500, 20)
-    slider.move(200, 57)
+    w = MainWin()
 
-    # Start button
-    sbtn = QtGui.QPushButton('Start Simulation', w)
-    sbtn.setToolTip('Click to start simulation')
-    sbtn.resize(sbtn.sizeHint())
-    sbtn.move(50, 50)
-    
-    # Quit button
-    qbtn = QtGui.QPushButton('Quit', w)
-    qbtn.setToolTip('Quit Application')
-    qbtn.resize(sbtn.sizeHint())
-    qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-    qbtn.move(WIDTH-200, HEIGHT-70)
-
-
-    
-    w.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
